36560454cd766dd582e7e4d5e83f5afc085912a1
Fayal Ahmmed <faysalahmmed177@gmail.com>
2021-05-05 12:28:12 +0600

    image issue and sidebar issue fixed

--- src/components/Dashboard/BookListCard/BookListCard.js
+++ src/components/Dashboard/BookListCard/BookListCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const BookListCard = ({ service }) => {
-    console.log(service.service.image.img);
+    //console.log(service.service.image.img);
     return (
-        <div className="col-md-4">
+        <div className="col-md-4 mb-5">
             <div className="card" style={{backgroundColor: 'lightgray', padding: '10px'}}>
-                <img className="text-center" src={`data:image/png;base64,${service.service.image.img}`} style={{ height: '100px', width: '150px'}} alt="" />
+                <img className="text-center" src={`data:image/png;base64,${service.service?.image?.img}`} style={{ height: '100px', width: '150px'}} alt="" />
                 <div>
                 <h4 className="mb-3">{service.service.name}</h4>
                 <p className="mb-3">{service.service.description}</p>
